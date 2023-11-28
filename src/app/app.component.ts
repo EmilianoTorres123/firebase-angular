6e273782f3ccd483eccffa57f81bd82fd93b4fa4
emiliano <106941242+EmilianoTorres123@users.noreply.github.com>
2023-11-27 21:50:27 -0600

    subir fotos a firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StorageService } from './services/storage.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firebase-angular';
+
+  constructor(private storageService:StorageService){
+
+  }
+  imagenes:any [] = [];
+  cargarImagen(event:any){
+    let archivos = event.target.files
+    let reader =new FileReader();
+    let nombre ="jonathan";
+
+    reader.readAsDataURL(archivos[0]);
+    reader.onloadend = () => {
+
+      console.log(reader.result);
+      this.imagenes.push(reader.result);
+      this.storageService.subirImagen(nombre+"_"+Date.now(), reader.result).then(urlImagen=>{
+        console.log(urlImagen);
+      });
+    }
+    //console.log(event.target.files);
+
+  }
+ 
+
+
+  
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDnP5lky0Ko8SjDhHLfj31gND16YtfRBEA",
+    authDomain: "arebotfitnes.firebaseapp.com",
+    projectId: "arebotfitnes",
+    storageBucket: "arebotfitnes.appspot.com",
+    messagingSenderId: "1029302101854",
+    appId: "1:1029302101854:web:570c9fbaf7333df1a051cf"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
